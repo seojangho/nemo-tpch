14b2d405a4dae1270e900f22e32b575213511246
sanha <sanhaleehana@naver.com>
2018-09-29 12:13:33 +0900

    increase io thread to 30

--- conf/src/main/java/org/apache/nemo/conf/JobConf.java
+++ conf/src/main/java/org/apache/nemo/conf/JobConf.java
@@ -179,7 +179,7 @@ public final class JobConf extends ConfigurationModuleBuilder {
    * Number of I/O threads for block fetch requests from other executor.
    */
   @NamedParameter(doc = "Number of I/O threads for block fetch request.", short_name = "io_request_threads",
-      default_value = "5")
+      default_value = "30")
   public final class IORequestHandleThreadsTotal implements Name<Integer> {
   }
 
